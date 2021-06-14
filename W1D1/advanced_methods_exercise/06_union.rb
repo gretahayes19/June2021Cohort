9671e7d93dd9e263b03b06e982694ea44a1103f3
Greta <gretahayes19@gmail.com>
2021-06-14 15:21:18 -0400

    advanced methods

--- W1D1/advanced_methods_exercise/06_union.rb
+++ W1D1/advanced_methods_exercise/06_union.rb
@@ -1,6 +1,15 @@
 # Write a method, union, that accepts any number of arrays as arguments.
 # The method should return an array containing all elements of the given arrays.
 
+require "byebug" 
+
+def union(*arrays)
+    arrays.inject do |acc, ele| 
+        debugger
+        acc + ele
+    end
+end
+
 
 p union(["a", "b"], [1, 2, 3]) # => ["a", "b", 1, 2, 3]
 p union(["x", "y"], [true, false], [20, 21, 23]) # => ["x", "y", true, false, 20, 21, 23]
