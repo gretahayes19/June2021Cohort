3e909bf6726b524df2bd7a32a4dff0312332a1f9
Greta <gretahayes19@gmail.com>
2021-06-16 12:03:22 -0400

    nauseating nums

--- W1D3/nauseating_nums/nauseating_nums.rb
+++ W1D3/nauseating_nums/nauseating_nums.rb
@@ -1,3 +1,4 @@
+## PHASE 1
 def strange_sums(arr)
     count = 0
 
@@ -54,4 +55,137 @@ end
 def perfect_square 
     sqrt = Math.sqrt(num).to_i 
     sqrt * sqrt == num
+end
+
+##PHASE 2
+
+
+
+def anti_prime?(targ)
+    amount = num_factors(targ)
+    (1...targ).all? { |i| amount > num_factors(i) }
+end
+
+def num_factors(num)
+    (1..num).count { |n| num % n == 0 }
+end
+
+# p anti_prime?(24)   # true
+# p anti_prime?(36)   # true
+# p anti_prime?(48)   # true
+# p anti_prime?(360)  # true
+# p anti_prime?(1260) # true
+# p anti_prime?(27)   # false
+# p anti_prime?(5)    # false
+# p anti_prime?(100)  # false
+# p anti_prime?(136)  # false
+# p anti_prime?(1024) # false
+
+
+def matrix_addition(m1, m2)
+    rows = m1.length
+    cols = m1[0].length
+    result = Array.new(rows) { Array.new(cols, 0)}
+    (0...rows).each do |row|
+        (0...cols).each do |col|
+            result[row][col] = m1[row][col] + m2[row][col]
+        end
+    end
+    
+    result
+end
+
+# matrix_a = [[2,5], [4,7]]
+# matrix_b = [[9,1], [3,0]]
+# matrix_c = [[-1,0], [0,-1]]
+# matrix_d = [[2, -5], [7, 10], [0, 1]]
+# matrix_e = [[0 , 0], [12, 4], [6,  3]]
+
+# p matrix_addition(matrix_a, matrix_b) # [[11, 6], [7, 7]]
+# p matrix_addition(matrix_a, matrix_c) # [[1, 5], [4, 6]]
+# p matrix_addition(matrix_b, matrix_c) # [[8, 1], [3, -1]]
+# p matrix_addition(matrix_d, matrix_e) # [[2, -5], [19, 14], [6, 4]]
+
+def mutual_factors(*nums)
+    nums
+        .map { |n| factors(n) }
+        .inject(:&)
+end
+
+def factors(num)
+    (1..num).select { |i| num % i == 0}
+end
+
+# p mutual_factors(8, 4)              # [1, 2, 4]
+# p mutual_factors(8, 4, 10)          # [1, 2]
+# p mutual_factors(12, 24)            # [1, 2, 3, 4, 6, 12]
+# p mutual_factors(12, 24, 64)        # [1, 2, 4]
+# p mutual_factors(50, 30)            # [1, 2, 5, 10]
+# p mutual_factors(50, 30, 45, 105)   # [1, 5]
+# p mutual_factors(22, 44)            # [1, 2, 11, 22]
+# p mutual_factors(22, 44, 11)        # [1, 11]
+# p mutual_factors(7)                 # [1, 7]
+# p mutual_factors(7, 9)              # [1]
+
+def tribonacci_number(n) 
+    return 1 if n == 1 || n == 2
+
+    return 2 if n == 3
+
+    return tribonacci_number(n-1) + tribonacci_number(n-2) + tribonacci_number(n-3)
+end
+
+# p tribonacci_number(1)  # 1
+# p tribonacci_number(2)  # 1
+# p tribonacci_number(3)  # 2
+# p tribonacci_number(4)  # 4
+# p tribonacci_number(5)  # 7
+# p tribonacci_number(6)  # 13
+# p tribonacci_number(7)  # 24
+# p tribonacci_number(11) # 274
+
+require "byebug"
+def matrix_addition_reloaded(*matricies)
+    matrix = matricies.first
+    height = matrix.length
+    width = matrix[0].length
+
+    empty_matrix = Array.new(height) { [0] * width }
+    matricies.inject(empty_matrix) do |m1, m2|
+        # debugger
+        return nil if m2.length != height or m2[0].length != width
+        matrix_addition(m1, m2)
+    end
+end
+
+matrix_a = [[2,5], [4,7]]
+matrix_b = [[9,1], [3,0]]
+matrix_c = [[-1,0], [0,-1]]
+matrix_d = [[2, -5], [7, 10], [0, 1]]
+matrix_e = [[0 , 0], [12, 4], [6,  3]]
+# p matrix_addition_reloaded(matrix_a, matrix_b)              # [[11, 6], [7, 7]]
+# p matrix_addition_reloaded(matrix_a, matrix_b, matrix_c)    # [[10, 6], [7, 6]]
+# p matrix_addition_reloaded(matrix_e)                        # [[0, 0], [12, 4], [6, 3]]
+# p matrix_addition_reloaded(matrix_d, matrix_e)              # [[2, -5], [19, 14], [6, 4]]
+# p matrix_addition_reloaded(matrix_a, matrix_b, matrix_e)    # nil
+# p matrix_addition_reloaded(matrix_d, matrix_e, matrix_c)    # nil
+
+
+def squarocol?(grid)
+    return true if grid.any? { |row| row.uniq.length == 1 }
+    return true if grid.transpose.any? { |col| col.uniq.length == 1 }
+    false
+end
+
+def squaragonal(grid)
+    size = grid.length - 1
+    diag_one = []
+    (0..size).each { |idx| diag_one.push(grid[idx][idx]) }
+    return true if diag_one.uniq.length == 1
+    
+    diag_two = []
+    (0..size).each { |idx| diag_two.push(grid[idx][size - idx]) }
+    return true if diag_two.uniq.length == 1
+
+    false
 end
