cd318d36c2b9da4bb7cce650b35cfed662569ca1
Greta <gretahayes19@gmail.com>
2021-06-16 09:56:55 -0400

    rampant_repeats_

--- W1D3/nauseating_nums/nauseating_nums.rb
+++ W1D3/nauseating_nums/nauseating_nums.rb
@@ -33,3 +33,20 @@ end
 # p pair_product([4, 2, 5, 7], 16)    # false
 # p pair_product([8, 4, 9, 3], 8)     # false
 # p pair_product([3], 12)             # false
+
+def rampant_repeats(string, hash)
+    new_str = ""
+    string.each_char do |char| 
+        if hash[char]
+            hash[char].times { new_str += char }
+        else
+            new_str += char
+        end
+    end
+    new_str
+end
+
+p rampant_repeats('taco', {'a'=>3, 'c'=>2})             # 'taaacco'
+p rampant_repeats('feverish', {'e'=>2, 'f'=>4, 's'=>3}) # 'ffffeeveerisssh'
+p rampant_repeats('misispi', {'s'=>2, 'p'=>2})          # 'mississppi'
+p rampant_repeats('faarm', {'e'=>3, 'a'=>2})            # 'faaaarm'
