fe5501b526ebd73dc2b257b5702b0152ee169068
Greta <gretahayes19@gmail.com>
2021-06-17 12:04:39 -0400

    perilous procs 2

--- W1D4/perilous_procs.rb
+++ W1D4/perilous_procs.rb
@@ -43,3 +43,63 @@ end
 # p filter_out([1, 7, 3, 5 ]) { |x| x.odd? }          # []
 # p filter_out([10, 6, 3, 2, 5 ]) { |x| x.even? }     # [3, 5]
 # p filter_out([1, 7, 3, 5 ]) { |x| x.even? }         # [1, 7, 3, 5]
+
+
+def at_least?(array, n, &prc)
+    count = 0
+    array.each do |el|
+        count += 1 if prc.call(el)
+    end
+    count >= n
+end
+
+# p at_least?(['sad', 'quick', 'timid', 'final'], 2) { |s| s.end_with?('ly') }
+# # false
+# p at_least?(['sad', 'quickly', 'timid', 'final'], 2) { |s| s.end_with?('ly') }
+# # false
+# p at_least?(['sad', 'quickly', 'timidly', 'final'], 2) { |s| s.end_with?('ly') }
+# # true
+# p at_least?(['sad', 'quickly', 'timidly', 'finally'], 2) { |s| s.end_with?('ly') }
+# # true
+# p at_least?(['sad', 'quickly', 'timid', 'final'], 1) { |s| s.end_with?('ly') }
+# # true
+# p at_least?(['sad', 'quick', 'timid', 'final'], 1) { |s| s.end_with?('ly') }
+# # false
+# p at_least?([false, false, false], 3) { |bool| bool }
+# # false
+# p at_least?([false, true, true], 3) { |bool| bool }
+# # false
+# p at_least?([true, true, true], 3) { |bool| bool }
+# # true
+# p at_least?([true, true, true, true], 3) { |bool| bool }
+# # true
+
+def every?(array, &prc)
+    array.each do |el|
+        return false if !prc.call(el)
+    end
+    true
+end
+
+# p every?([3, 1, 11, 5]) { |n| n.even? }                                 # false
+# p every?([2, 4, 4, 8]) { |n| n.even? }                                  # true
+# p every?([8, 2]) { |n| n.even? }                                        # true
+# p every?(['squash', 'corn', 'kale', 'carrot']) { |str| str[0] == 'p' }  # false
+# p every?(['squash', 'pea', 'kale', 'potato']) { |str| str[0] == 'p' }   # false
+# p every?(['parsnip', 'potato', 'pea']) { |str| str[0] == 'p' }          # true
+
+def at_most?(array, n, &prc)
+    count = 0
+    array.each do |el|
+        count += 1 if prc.call(el)
+    end
+    count <= n
+end
+
+# p at_most?([-4, -100, -3], 1) { |el| el > 0 }                        # true
+# p at_most?([-4, 100, -3], 1) { |el| el > 0 }                         # true
+# p at_most?([4, 100, -3], 1) { |el| el > 0 }                          # false
+# p at_most?([4, 100, 3], 1) { |el| el > 0 }                           # false
+# p at_most?(['r', 'q', 'e', 'z'], 2) { |el| 'aeiou'.include?(el) }    # true
+# p at_most?(['r', 'i', 'e', 'z'], 2) { |el| 'aeiou'.include?(el) }    # true
+# p at_most?(['r', 'i', 'e', 'o'], 2) { |el| 'aeiou'.include?(el) }    # false
