9eb74f749cebf5a6526d63215aa2989a94db27ee
Greta <gretahayes19@gmail.com>
2021-06-18 10:11:34 -0400

    more meddling

--- W1/W1D5/meddling_medley.rb
+++ W1/W1D5/meddling_medley.rb
@@ -54,3 +54,119 @@ end
 # first = Proc.new { |a| a[0] }
 # p hash_mapped({-5=>['q', 'r', 's'], 6=>['w', 'x']}, first) { |n| n * n }
 # # {25=>"q", 36=>"w"}
+
+
+
+def counted_characters(string)
+    count = Hash.new(0)
+    string.each_char { |char| count[char] += 1 }
+    count
+        .select { |char, num| num > 2 }
+        .keys
+end
+
+# p counted_characters("that's alright folks") # ["t"]
+# p counted_characters("mississippi") # ["i", "s"]
+# p counted_characters("hot potato soup please") # ["o", "t", " ", "p"]
+# p counted_characters("runtime") # []
+
+
+def triplet_true?(string)
+    (0...string.length - 2).each do |i|
+        if string[i..i+2] == string[i] * 3
+            return true
+        end
+    end
+
+    false
+end
+
+# p triplet_true?('caaabb')        # true
+# p triplet_true?('terrrrrible')   # true
+# p triplet_true?('runninggg')     # true
+# p triplet_true?('bootcamp')      # false
+# p triplet_true?('e')             # false
+
+def energetic_encoding(string, h)
+    new_string = ''
+    string.each_char do |char|
+        if char == ' '
+            new_string += ' '
+        elsif h.has_key?(char)
+            new_string += h[char]
+        else
+            new_string += '?'
+        end
+    end
+    new_string
+end
+
+# p energetic_encoding('sent sea',
+#     'e'=>'i', 's'=>'z', 'n'=>'m', 't'=>'p', 'a'=>'u'
+# ) # 'zimp ziu'
+
+# p energetic_encoding('cat',
+#     'a'=>'o', 'c'=>'k'
+# ) # 'ko?'
+
+# p energetic_encoding('hello world',
+#     'o'=>'i', 'l'=>'r', 'e'=>'a'
+# ) # '?arri ?i?r?'
+
+# p energetic_encoding('bike', {}) # '????'
+
+def uncompress(str)
+    uncompressed = ''
+    (0...str.length - 1).each do |i|
+        letter = str[i]
+        num = str[i + 1].to_i
+        debugger
+        uncompressed += letter * num
+    end
+    uncompressed
+end
+
+# p uncompress('a2b4c1') # 'aabbbbc'
+# p uncompress('b1o2t1') # 'boot'
+# p uncompress('x3y1x2z4') # 'xxxyxxzzzz'
+
+def conjunct_select(array, *prcs)
+    array.select { |el| prcs.all? { |prc| prc.call(el) } }
+end
+
+def conjunct_select(array, *prcs)
+    prcs.inject(array) { |acc, prc| acc.select(&prc) }
+end
+
+# is_positive = Proc.new { |n| n > 0 }
+# is_odd = Proc.new { |n| n.odd? }
+# less_than_ten = Proc.new { |n| n < 10 }
+# p conjunct_select([4, 8, -2, 11, 7, -3, 13], is_positive) # [4, 8, 11, 7, 13]
+# p conjunct_select([4, 8, -2, 11, 7, -3, 13], is_positive, is_odd) # [11, 7, 13]
+# p conjunct_select([4, 8, -2, 11, 7, -3, 13], is_positive, is_odd, less_than_ten) # [7]
+
+
+def convert_pig_latin(sentence)
+    words = sentence.split(' ')
+    new_words = words.map do |word|
+        new_word = word.length > 2 ? convert_word(word) : word
+        word == word.capitalize ? new_word.capitalize : new_word
+    end
+    new_words.join(' ')
+end
+
+def convert_word(word)
+    vowels = 'AEIOUaeiou'
+    return word + 'yay' if vowels.include?(word[0])
+    word.each_char.with_index do |char, i|
+        if vowels.include?(char)
+            return word[i..-1] + word[0...i] + 'ay'
+        end
+    end
+end
+
+# p convert_pig_latin('We like to eat bananas') # "We ikelay to eatyay ananasbay"
+# p convert_pig_latin('I cannot find the trash') # "I annotcay indfay ethay ashtray"
+# p convert_pig_latin('What an interesting problem') # "Atwhay an interestingyay oblempray"
+# p convert_pig_latin('Her family flew to France') # "Erhay amilyfay ewflay to Ancefray"
+# p convert_pig_latin('Our family flew to France') # "Ouryay amilyfay ewflay to Ancefray"
